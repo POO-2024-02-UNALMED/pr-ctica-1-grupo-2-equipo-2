34bac26ab5fe2b872d83fa04809502fa7d967739
JuanCamilo268 <juleonba@unal.edu.co>
2025-01-15 07:22:36 -0500

    Create Empleado.java

---
+++ nuevaSucursal/Empleado.java
@@ -0,0 +1,11 @@
+package nuevaSucursal;
+
+public class Empleado {
+	protected String nombre;
+	protected long cedula;
+	
+	protected Empleado(String nombre, long cedula) {
+		this.nombre = nombre;
+		this.cedula = cedula;
+	}
+}

a8a43428f34d1f9bd5685cdf9440b8336ec8d746
JuanCamilo268 <juleonba@unal.edu.co>
2025-01-17 09:38:06 -0500

    Update Empleado.java

--- nuevaSucursal/Empleado.java
+++ nuevaSucursal/Empleado.java
@@ -1,11 +1,6 @@
 package nuevaSucursal;
 
-public class Empleado {
-	protected String nombre;
-	protected long cedula;
-	
-	protected Empleado(String nombre, long cedula) {
-		this.nombre = nombre;
-		this.cedula = cedula;
-	}
+public abstract class Empleado {
+
+
 }

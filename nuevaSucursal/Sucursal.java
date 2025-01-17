6ad77a813654ee61d7979d4592e4f89a1341c1d3
JuanCamilo268 <juleonba@unal.edu.co>
2025-01-17 09:37:53 -0500

    Update Sucursal.java

--- nuevaSucursal/Sucursal.java
+++ nuevaSucursal/Sucursal.java
@@ -1,5 +1,19 @@
 package nuevaSucursal;
 
-public class Sucursal  {
-	
+import ordenFisica.Mesa;
+import ordenFisica.Mesero;
+
+public class Sucursal {
+	private int id;
+    private String nombre;
+    private Mesa[] mesas;
+    private Mesero[] meseros;
+    private double presupuesto;
+
+    public Sucursal(int id, String nombre, int cantidad){
+        this.id = id;
+        this.nombre = nombre;
+        this.mesas = new Mesa[cantidad];
+        this.meseros = new Mesero[cantidad];
+    }
 }

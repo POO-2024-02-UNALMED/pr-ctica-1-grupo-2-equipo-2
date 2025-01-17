c176480c67a7d1624296650e5577487ab18a33f5
JuanCamilo268 <juleonba@unal.edu.co>
2025-01-17 09:37:55 -0500

    Create Mesero.java

---
+++ nuevaSucursal/Mesero.java
@@ -0,0 +1,12 @@
+package nuevaSucursal;
+
+import ordenFisica.Mesero;
+
+public class Mesero extends Empleado {
+    private String nombre;
+    private long id;
+
+    public Mesero(int id, String nombre){
+
+    }
+}

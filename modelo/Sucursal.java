69a26aa57ad10f64b9b2548bb44881c796e948af
JuanCamilo268 <juleonba@unal.edu.co>
2025-01-19 17:05:02 -0500

    Update Sucursal.java

--- modelo/Sucursal.java
+++ modelo/Sucursal.java
@@ -1,8 +1,9 @@
 package modelo;
 
-
-import ordenFisica.Empleado;
+import java.util.ArrayList;
+import java.util.Scanner;
 import ordenFisica.Ingrediente;
+import java.util.List;
 
 public class Sucursal {
 	private int id;
@@ -10,6 +11,9 @@ public class Sucursal {
 	private int presupuesto;
 	private Ingrediente[] inventario;
 	private Empleado[] empleados;
+    private Mesero[] meseros;
+    private Mesa[] mesas;
+    private int[] direccion;
 	
 	
 	public Sucursal(int id, String ubicacion,int presupuesto,Ingrediente[] inventario,Empleado[] empleados) {
@@ -21,6 +25,58 @@ public class Sucursal {
 	}
     public int getId() {
         return id;
+    }
+
+    public Sucursal(int id, String nombre, int cantidad, int[] direccion){
+        this.id = id;
+        this.ubicacion = nombre;
+        this.mesas = new Mesa[cantidad];
+        this.meseros = new Mesero[cantidad];
+        this.direccion = direccion;
+    }
+
+    public static void comprarTerreno(double presupuesto, List<Sucursal> sucursal) {
+        Barrio[] candidatos = Barrio.getCiudad();
+        Barrio[] hay = new Barrio[16];
+        Barrio[] noHay = new Barrio[16];
+        int si = 0;
+        int no = 0;
+        for(Barrio barrio: candidatos) {
+            if (barrio.tieneRestaurante()) {hay[si] = barrio; si++;}
+            else {noHay[no] = barrio; no++;}
+        }
+        System.out.println("Escoja en cuál barrio desea abrir la nueva sucursal");
+        for (int i = 0; i < no; i++) {
+            if (noHay == null) {break;}
+            System.out.println((i+1) + ". " + noHay[i]);
+        }
+        Scanner scanner = new Scanner(System.in);
+        int eleccion = scanner.nextInt();
+        while (eleccion > no) {
+            System.out.println("Opción no disponible");
+            System.out.println("Escoja otra opción");
+            scanner = new Scanner(System.in);
+            eleccion = scanner.nextInt();
+        }
+        Barrio barrio = noHay[no - 1];
+        ArrayList<Esquina> locales = barrio.getEsquinas();
+        int i = 0;
+        for (Esquina local: locales) {
+            System.out.println((i + 1) + ". " + local);
+            i++;
+        }
+        scanner = new Scanner(System.in);
+        eleccion = scanner.nextInt();
+        while (eleccion > i) {
+            System.out.println("Opción no disponible");
+            System.out.println("Escoja otra opción");
+            scanner = new Scanner(System.in);
+            eleccion = scanner.nextInt();
+        }
+        boolean correcto = false;
+        while (correcto == false) {
+
+        }
     }
 
     public void setId(int id) {

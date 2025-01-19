dc2a3b5248ebc2ed9ae49f99ae135d889b7c4ce5
JuanCamilo268 <juleonba@unal.edu.co>
2025-01-19 17:03:57 -0500

    Update Mesa.java

---
+++ modelo/Mesa.java
@@ -0,0 +1,51 @@
+package modelo;
+
+public class Mesa {
+	private int id;
+	private int capacidad;
+	private Sucursal sucursal;
+	private boolean unida;
+	
+	public Mesa(int id,int capacidad,Sucursal sucursal) {
+		this.id=id;
+		this.capacidad=capacidad;
+		this.sucursal=sucursal;
+		this.unida= false;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id=id;
+	}
+	
+	public int getCapacidad() {
+		return capacidad;
+	}
+	
+	public void setCapacidad(int capacidad) {
+		this.capacidad=capacidad;
+	}
+	
+	public Sucursal getSucursal() {
+		return sucursal;
+	}
+	
+	public void setSucursal(Sucursal sucursal) {
+		this.sucursal=sucursal;
+	}
+	
+	public boolean getUnida() {
+		return unida;
+	}
+	
+	public void setUnida(boolean unida) {
+		this.unida=unida;
+	}
+	
+	public void UnirMesas(Mesa mesa1, Mesa mesa2) {
+		
+	}
+}

f579f356160f43e864b2b2bc5a673b285281cdc1
JuanCamilo268 <juleonba@unal.edu.co>
2025-01-24 21:27:25 -0500

    Update Empresa.java

--- modelo/Empresa.java
+++ modelo/Empresa.java
@@ -2,39 +2,59 @@ package modelo;
 
 import baseDatos.DataManager;
 import error.Entrada;
-
+import java.util.List;
 import java.io.Serializable;
 import java.util.ArrayList;
 
 
 public class Empresa implements Serializable{
-	private static final long serialVersionUID = 1L;
 	private static double deudas = 12000000;
-	private static double renta = 8000000;
-	private static double gastoRecursos;
+	private static double renta = 1000000;
+	private static double gastoRecursos = 15000000;
 	private static double gastoSalarios;
 	private static double presupuestoTotal;
 	private static double presupuestoBruto = 40000000;
 	private static double solvencia;
 	
 	private static void verFinanzas() {
-		System.out.println("Deudas crediticias: " + deudas + "\n" +
-				"Renta: " + renta + "\n" +
-				"Gasto en salarios: " + gastoSalarios + "\n" +
-				"Gasto en recursos: " + gastoRecursos + "\n" +
-				"Capital bruto: " + presupuestoBruto + "\n" +
-				"Capital neto: " + deudas + "\n" +
+		System.out.println("Deudas crediticias: " + deudas/1000000 + "M\n" +
+				"Renta: " + renta/1000000 + "M\n" +
+				"Gasto en salarios: " + gastoSalarios/1000000 + "M\n" +
+				"Gasto en recursos: " + gastoRecursos/1000000 + "M\n" +
+				"Capital bruto: " + presupuestoBruto/1000000 + "M\n" +
+				"Capital neto: " + presupuestoTotal/1000000 + "M\n" +
 				"Solvencia: " + solvencia());
 	}
 
+
 	public static void setDeudas() {
-		Empresa.deudas = Entrada.input();
+		Empresa.deudas = 10000000;
 	}
 
-	private static void calcularFinanzas(){
+	private static void calcularFinanzas(DataManager dataManager){
+		List<Sucursal> sucursales = dataManager.getSucursales();
+		renta = 1000000 * sucursales.size();
+		gastoSalarios = 0;
+		gastoRecursos = 0;
+		for (Sucursal sucursal: sucursales){
+			for(Empleado empleado: sucursal.getEmpleado()){
+				if(empleado == null){continue;}
+				gastoSalarios += empleado.getSueldo() * 12;
+			}
+			for(Mesero mesero: sucursal.getMeseros()){
+				if(mesero == null){continue;}
+				gastoSalarios += mesero.getSueldo() * 12;
+			}
+			gastoRecursos += sucursal.getGastoRecursos();
+			presupuestoBruto = sucursal.getPresupuesto();
+		}
+		for(Repartidor repartidor: dataManager.getRepartidores()) {
+			gastoSalarios += 1000000 + (1000000 * (repartidor.getCalificacionPromedio() / 100));
+		}
+		presupuestoTotal = presupuestoBruto - renta - gastoRecursos;
 	}
 	private static double solvencia() {
-		double pasivos = deudas + renta + gastoRecursos + gastoSalarios;
+		double pasivos = deudas + renta + gastoRecursos;
 		double activos = presupuestoBruto;
 		return activos/pasivos;
 	}
@@ -64,15 +84,15 @@ public class Empresa implements Serializable{
 	}
 	public static void menuFinanzas(DataManager dataManager) {
 		boolean salir = false;
+		calcularFinanzas(dataManager);
 		while (salir == false) {
 			System.out.println("=== Menú Finanzas ===");
 			System.out.println("¿Qué acción desea realizar");
 			System.out.println("1. Ver finanzas generales");
-			System.out.println("2. Calcular finanzas");
-			System.out.println("3. Ver sucursales");
-			System.out.println("4. Abrir sucursal");
-			System.out.println("5. Cerrar sucursal");
-			System.out.println("6. Salir");
+			System.out.println("2. Ver sucursales");
+			System.out.println("3. Abrir sucursal");
+			System.out.println("4. Cerrar sucursal");
+			System.out.println("5. Salir");
 
 			int eleccion = Entrada.input();
 
@@ -80,10 +100,10 @@ public class Empresa implements Serializable{
 				case 1:
 					verFinanzas();
 					break;
-				case 3:
+				case 2:
 					verSucursales(dataManager);
 					break;
-				case 4:
+				case 3:
 					double presupuesto = pedirPrestamo();
 					if(presupuesto == 0){
 						System.out.println("No se ha concretado ningún préstamo");
@@ -91,14 +111,24 @@ public class Empresa implements Serializable{
 					}
 					Sucursal newSucursal = Barrio.comprarTerreno(presupuesto, dataManager.getSucursales(), dataManager.getCiudad());
 					newSucursal.comprarMesas(presupuesto,newSucursal.getEspacio());
+					for(int i = 0; i < 5; i++){
+						boolean correcto = false;
+						while(!correcto){
+							int cedula = Empleado.generarDocumento();
+							correcto = dataManager.explorar(cedula);
+							newSucursal.autoEmpleado(i,cedula);
+						}
+
+					}
 					dataManager.addSucursal(newSucursal);
+					Administrativo.nuevoAdmin(dataManager);
 					System.out.println(newSucursal);
 					break;
-				case 6:
-					salir = true;
+				case 4:
+					dataManager.quitarSucursal();
 					break;
-				case 7:
-					setDeudas();
+				case 5:
+					salir = true;
 					break;
 				default:
 					System.out.println("Opción no disponible");
